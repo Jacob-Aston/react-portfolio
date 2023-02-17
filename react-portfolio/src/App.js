c44277c76bd323163e72aa0e3f8124568e70ad40
JAston <jacob.aston1@gmail.com>
2023-02-16 22:27:33 -0700

    got page functioning with no real content

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from './components/Header';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navigation />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
