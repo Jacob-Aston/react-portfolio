ec1f535d565908be147ff22db4b5f842f40afd27
JAston <jacob.aston1@gmail.com>
2023-02-21 17:17:38 -0700

    added links to footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -3,9 +3,15 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <div>Icon</div>
-      <div>Icon</div>
-      <div>Icon</div>
+      <div>
+      <a href="https://github.com/Jacob-Aston">Github</a>
+      </div>
+      <div>
+      <a href="www.linkedin.com/in/jacob-aston-69bb57265">LinkedIn</a>
+      </div>
+      <div>
+      <a href="https://twitter.com/_Jacob_Aston">Twitter</a>
+      </div>
     </div>
   );
 }
