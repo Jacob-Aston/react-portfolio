9776c5ea1c2bef34d8c67b1b47bdfa16ed199d00
JAston <jacob.aston1@gmail.com>
2023-02-21 17:20:03 -0700

    fixed linkedIn link

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <a href="https://github.com/Jacob-Aston">Github</a>
       </div>
       <div>
-      <a href="www.linkedin.com/in/jacob-aston-69bb57265">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/jacob-aston-69bb57265/">LinkedIn</a>
       </div>
       <div>
       <a href="https://twitter.com/_Jacob_Aston">Twitter</a>
