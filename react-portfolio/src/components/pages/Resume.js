a93632a62cf332aaa77a6ccc80ef3ffea3e41467
JAston <jacob.aston1@gmail.com>
2023-02-22 21:02:46 -0700

    added resume content

--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 export default function Resume() {
-    return(
-        <div></div>
-    )
+  return (
+    <div>
+      <p>
+        Take a look at my previous work experience. It may not be tech related
+        but I sure learned how to work hard along with the importance of supporting my
+        team.
+      </p>
+      <a href="https://docs.google.com/document/d/15glYOBtjn69N-r6MAXLW3JROV8M4GcMigGGyeDuWnaQ/edit?usp=sharing">
+        Resume
+      </a>
+      <p>Along with my previous work experience here is a list of what we covered in my programming course.</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Web Apis</li>
+        <li>Third Party APIs</li>
+        <li>Server Side Apis</li>
+        <li>NodeJS</li>
+        <li>Object Oriented Programming</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>ORM</li>
+        <li>MVC</li>
+        <li>CS</li>
+        <li>NoSQL</li>
+        <li>PWA</li>
+        <li>React</li>
+        <li>MERN</li>
+      </ul>
+    </div>
+  );
 }
