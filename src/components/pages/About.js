9f1daff1c3b6ef7a56923156c727534eb28adc1e
JAston <jacob.aston1@gmail.com>
2023-03-04 21:22:03 -0700

    fixed file structure

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <p>I'm a strapping young lad with an interest in entrepreneurship and leadership. Through my time Working I have learned the importance of developing a positive culture in the workplace. As well as doing what I can to support the team and leadership.</p>
+        </div>
+    );
+}
