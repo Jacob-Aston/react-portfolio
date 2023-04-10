0e19f1385ccd67250ad7ba868509bb18e7f5a4a4
JAston <jacob.aston1@gmail.com>
2023-04-09 19:49:44 -0600

    added nav dropdown jsx and css for mobile first on about page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,37 +2,53 @@ import React from "react";
 import "../styles/Navigation.css";
 
 function Navigation({ currentPage, handlePageChange }) {
+const dropFunction = () => {
+  
+}
   return (
-    <ul className="nav">
-      <a
-        href="#about"
-        onClick={() => handlePageChange("About")}
-        className={currentPage === "About" ? "nav-link active" : "nav-link"}
-      >
-        About Me
-      </a>
-      <a
-        href="#portfolio"
-        onClick={() => handlePageChange("Portfolio")}
-        className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
-      >
-        Portfolio
-      </a>
-      <a
-        href="#contact"
-        onClick={() => handlePageChange("Contact")}
-        className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-      >
-        Contact
-      </a>
-      <a
-        href="#resume"
-        onClick={() => handlePageChange("Resume")}
-        className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
-      >
-        Resume
-      </a>
-    </ul>
+    <div>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={dropFunction}>Dropdown</button>
+        <div className="dropdown-content" id="myDropdown">
+          <div>About Me</div>
+          <div>Portfolio</div>
+          <div>Contact</div>
+          <div>Resume</div>
+        </div>
+      </div>
+      <ul className="nav">
+        <a
+          href="#about"
+          onClick={() => handlePageChange("About")}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange("Portfolio")}
+          className={
+            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </a>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+        <a
+          href="#resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </ul>
+    </div>
   );
 }
 
