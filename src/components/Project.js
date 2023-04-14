cccc70c6856b6ee5b8ad7e7db40f0412c1bb52c4
JAston <jacob.aston1@gmail.com>
2023-04-14 00:13:29 -0600

    mobile design for project page. changed project link variables and moved links and title off of image.

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,22 +6,26 @@ export default function Project(props) {
     <ul>
       {props.projects.map((project) => (
         <div className="project-container" key={project.id}>
-          <img className="project-image" src={project.image} alt=""></img>
-          <div className="project-title">{project.title}</div>
-          {project.deployed ? (
-            <a className="project-deployed" href={project.deployed}>
-              {project.deployedText}
-            </a>
-          ) : (
-            <div></div>
-          )}
-          {project.repo ? (
-            <a className="project-repo" href={project.repo}>
-              {project.repoText}
-            </a>
-          ) : (
-            <div></div>
-          )}
+          <div className="image-container">
+            <img className="project-image" src={project.image} alt=""></img>
+            <div className="project-title">{project.title}</div>
+          </div>
+          <div className="link-container">
+            {project.linkOne ? (
+              <a className="project-link" href={project.linkOne}>
+                {project.linkOneText}
+              </a>
+            ) : (
+              <div></div>
+            )}
+            {project.linkTwo ? (
+              <a className="project-link" href={project.linkTwo}>
+                {project.linkTwoText}
+              </a>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
       ))}
     </ul>
--- src/components/projectList.js
+++ src/components/projectList.js
@@ -2,17 +2,17 @@ const projects = [
   {
     image: require("./../assets/images/JATE.png"),
     title: "Just Another Text Editor",
-    deployed: "https://texty-rexy.herokuapp.com/",
-    deployedText: "Deployed App",
-    repo: "https://github.com/Jacob-Aston/texty-rexy",
-    repoText: "Repo",
+    linkOne: "https://texty-rexy.herokuapp.com/",
+    linkOneText: "Deployed App",
+    linkTwo: "https://github.com/Jacob-Aston/texty-rexy",
+    linkTwoText: "Repo",
     id: "1",
   },
   {
     image: require("./../assets/images/WeatherForecast.png"),
     title: "Weather Forecast",
-    repo: "https://github.com/Jacob-Aston/weather-forecast",
-    repoText: "Repo",
+    linkOne: "https://github.com/Jacob-Aston/weather-forecast",
+    linkOneText: "Repo",
     id: "2",
   },
   {
@@ -23,10 +23,10 @@ const projects = [
   {
     image: require("./../assets/images/Stats-Tasktics.png"),
     title: "Stats-Tasktics",
-    repo: "https://github.com/Jacob-Aston/stats-tasktics",
-    repoText: "Repo",
-    deployed: "https://stat-tasktics.herokuapp.com/",
-    deployedText: "Deployed",
+    linkOne: "https://stat-tasktics.herokuapp.com/",
+    linkOneText: "Deployed",
+    linkTwo: "https://github.com/Jacob-Aston/stats-tasktics",
+    linkTwoText: "Repo",
     id: "4",
   },
 ];
