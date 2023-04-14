4e90c012e98e570715cc3d5417c7132b1e1031b4
JAston <jacob.aston1@gmail.com>
2023-04-13 19:46:07 -0600

    changed dropdown content to ul of a tags.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,12 +9,42 @@ function Navigation({ currentPage, handlePageChange }) {
         <button className="dropbtn" onClick={dropFunction}>
           ---
         </button>
-        <div className="dropdown-content" id="myDropdown">
-          <button>About Me</button>
-          <button>Portfolio</button>
-          <button>Contact</button>
-          <button>Resume</button>
-        </div>
+        <ul className="dropdown-content" id="myDropdown">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About Me
+          </a>
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+            }
+          >
+            Portfolio
+          </a>
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link active" : "nav-link"
+            }
+          >
+            Resume
+          </a>
+        </ul>
       </div>
       <ul className="nav">
         <a
