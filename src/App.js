0d513670351744c9f6680f612d19d5a6711559e8
JAston <jacob.aston1@gmail.com>
2023-04-17 00:33:48 -0600

    changed app background to white.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from './components/Header';
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import "./styles/App.css"
+import "./styles/App.css";
 
 function App() {
   return (
